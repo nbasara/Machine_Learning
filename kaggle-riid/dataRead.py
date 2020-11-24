ec1a0586ea70f3580d376d57538ad7991d687621
nbasara <52301957+nbasara@users.noreply.github.com>
2020-11-24 10:25:51 -0800

    Because of the large amount of data working in notebook will server best for efficiency

--- kaggle-riid/dataRead.py
+++ kaggle-riid/dataRead.py
@@ -1,11 +1,15 @@
 ###Reading in data and getting a visualization
 import pandas as pd 
 import matplotlib as plt
+import time 
 
+t0 = time.time()
+
+pd.options.display.max_rows = 2000
 #read in data
 data_path = './../../riid-test-answer-prediction/'
 ''' Possibble Data files:
-	train.csv
+	train.csv waring reading will take 158s
 	questions.csv
 	lectures.csv
 	example_test.csv
@@ -14,4 +18,8 @@ df = pd.read_csv(data_path + 'train.csv')
 
 print(df.head(10))
 
+t1 = time.time()
+
+total = t1-t0
+print(total)
 #visualize the data
